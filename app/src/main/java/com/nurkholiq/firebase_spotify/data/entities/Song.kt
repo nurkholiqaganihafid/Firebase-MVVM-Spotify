2eb0868a6189055f5943681be6e34aa6dada5dc2
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-15 14:24:38 +0700

    add new file data class Song sesuai nama field pada firestore

---
+++ app/src/main/java/com/nurkholiq/firebase_spotify/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.nurkholiq.firebase_spotify.data.entities
+
+data class Song(
+    val mediaId: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val songUrl: String = "",
+    val imageUrl: String = ""
+)
