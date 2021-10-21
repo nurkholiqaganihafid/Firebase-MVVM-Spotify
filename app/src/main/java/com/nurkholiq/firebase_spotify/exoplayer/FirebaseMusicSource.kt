1e2b067961584cc693a76a7c142979c614a1d9bd
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-21 11:47:31 +0700

    modified ServiceModule.kt and FirebaseMusicSource.kt untuk menerapkan daftar yang berisi media metadata compat objects

--- app/src/main/java/com/nurkholiq/firebase_spotify/exoplayer/FirebaseMusicSource.kt
+++ app/src/main/java/com/nurkholiq/firebase_spotify/exoplayer/FirebaseMusicSource.kt
@@ -1,8 +1,36 @@
 package com.nurkholiq.firebase_spotify.exoplayer
 
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.MediaMetadataCompat.*
+import com.nurkholiq.firebase_spotify.data.remote.MusicDatabase
 import com.nurkholiq.firebase_spotify.exoplayer.State.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class FirebaseMusicSource {
+class FirebaseMusicSource @Inject constructor(
+    private val musicDatabase: MusicDatabase
+) {
+
+    var songs = emptyList<MediaMetadataCompat>()
+
+    suspend fun fetchMediaData() = withContext(Dispatchers.IO) {
+        state = STATE_INITIALIZING
+        val allSongs = musicDatabase.getAllSongs()
+        songs = allSongs.map { song ->
+            MediaMetadataCompat.Builder()
+                .putString(METADATA_KEY_ARTIST, song.subtitle)
+                .putString(METADATA_KEY_MEDIA_ID, song.mediaId)
+                .putString(METADATA_KEY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_ICON_URI, song.imageUrl)
+                .putString(METADATA_KEY_MEDIA_URI, song.songUrl)
+                .putString(METADATA_KEY_ALBUM_ART_URI, song.imageUrl)
+                .putString(METADATA_KEY_DISPLAY_SUBTITLE, song.subtitle)
+                .putString(METADATA_KEY_DISPLAY_DESCRIPTION, song.subtitle)
+                .build()
+        }
+    }
 
     private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
 
