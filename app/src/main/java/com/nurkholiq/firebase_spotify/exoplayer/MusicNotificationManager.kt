04fd4f681a9cc9afea2325454868305a7646385d
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-22 13:31:30 +0700

    modified MusicNotificationManager.kt: membuat inner class DescriptionAdapter untuk implement keempat function

--- app/src/main/java/com/nurkholiq/firebase_spotify/exoplayer/MusicNotificationManager.kt
+++ app/src/main/java/com/nurkholiq/firebase_spotify/exoplayer/MusicNotificationManager.kt
@@ -1,8 +1,15 @@
 package com.nurkholiq.firebase_spotify.exoplayer
 
+import android.app.PendingIntent
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.PlayerNotificationManager
 import com.nurkholiq.firebase_spotify.R
 import com.nurkholiq.firebase_spotify.other.Constants.NOTIFICATION_CHANNEL_ID
@@ -42,5 +49,40 @@ class MusicNotificationManager(
         setChannelDescriptionResourceId(R.string.notification_channel_description)
         setNotificationListener(notificationListener)
     }.build()
+
+    private inner class DescriptionAdapter(
+        private val mediaController: MediaControllerCompat
+    ) : PlayerNotificationManager.MediaDescriptionAdapter {
+        override fun getCurrentContentTitle(player: Player): CharSequence {
+            return mediaController.metadata.description.title.toString()
+        }
+
+        override fun createCurrentContentIntent(player: Player): PendingIntent? {
+            return mediaController.sessionActivity
+        }
+
+        override fun getCurrentContentText(player: Player): CharSequence? {
+            return mediaController.metadata.description.subtitle.toString()
+        }
+
+        override fun getCurrentLargeIcon(
+            player: Player,
+            callback: PlayerNotificationManager.BitmapCallback
+        ): Bitmap? {
+            Glide.with(context).asBitmap()
+                .load(mediaController.metadata.description.iconUri)
+                .into(object : CustomTarget<Bitmap>() {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+                        callback.onBitmap(resource)
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) = Unit
+                })
+            return null
+        }
+    }
 
 }
