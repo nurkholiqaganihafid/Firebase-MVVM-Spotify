f3ed4e365d73012d92d9da04a1001e8fe085e27a
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-26 13:40:12 +0700

    create a new class MusicServiceConnection implement beberapa object data yang berisi informasi dari service

---
+++ app/src/main/java/com/nurkholiq/firebase_spotify/exoplayer/MusicServiceConnection.kt
@@ -0,0 +1,25 @@
+package com.nurkholiq.firebase_spotify.exoplayer
+
+import android.content.Context
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.nurkholiq.firebase_spotify.other.Event
+import com.nurkholiq.firebase_spotify.other.Resource
+
+class MusicServiceConnection(
+    context: Context
+) {
+    private val _isConnected = MutableLiveData<Event<Resource<Boolean>>>()
+    val isConnected: LiveData<Event<Resource<Boolean>>> = _isConnected
+
+    private val _networkError = MutableLiveData<Event<Resource<Boolean>>>()
+    val networkError: LiveData<Event<Resource<Boolean>>> = _networkError
+
+    private val _playbackState = MutableLiveData<PlaybackStateCompat?>()
+    val playbackState: LiveData<PlaybackStateCompat?> = _playbackState
+
+    private val _curPlayingSong = MutableLiveData<MediaMetadataCompat>()
+    val curPlayingSong: LiveData<MediaMetadataCompat> = _curPlayingSong
+}
