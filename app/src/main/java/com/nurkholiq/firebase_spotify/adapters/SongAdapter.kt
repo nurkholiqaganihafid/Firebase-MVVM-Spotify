d8e6f704f7f1781c1d2b1b68ae886ea038b4a460
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-28 13:47:39 +0700

    create layout list_item.xml and modified SongAdapter: menerapkan tiga fun dan implement click listener

--- app/src/main/java/com/nurkholiq/firebase_spotify/adapters/SongAdapter.kt
+++ app/src/main/java/com/nurkholiq/firebase_spotify/adapters/SongAdapter.kt
@@ -1,16 +1,22 @@
 package com.nurkholiq.firebase_spotify.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
+import com.nurkholiq.firebase_spotify.R
 import com.nurkholiq.firebase_spotify.data.entities.Song
+import com.nurkholiq.firebase_spotify.databinding.ListItemBinding
 import javax.inject.Inject
 
 class SongAdapter @Inject constructor(
     private val glide: RequestManager
 ) : RecyclerView.Adapter<SongAdapter.SongViewHolder>() {
+
+    private lateinit var binding: ListItemBinding
 
     class SongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
@@ -29,4 +35,39 @@ class SongAdapter @Inject constructor(
     var songs: List<Song>
         get() = differ.currentList
         set(value) = differ.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        return SongViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.list_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songs[position]
+        holder.itemView.apply {
+            binding.tvPrimary.text = song.title
+            binding.tvSecondary.text = song.subtitle
+            glide.load(song.imageUrl).into(binding.ivItemImage)
+
+            setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((Song) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Song) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun getItemCount(): Int {
+        return songs.size
+    }
 }
