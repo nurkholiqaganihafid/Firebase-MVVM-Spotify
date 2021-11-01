39c65b7abc1619203cccc9c69921459a6eb6d940
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-11-01 15:12:49 +0700

    create observed lain jika playing song. maka, simbol pause akan muncul and otherwise

--- app/src/main/java/com/nurkholiq/firebase_spotify/ui/MainActivity.kt
+++ app/src/main/java/com/nurkholiq/firebase_spotify/ui/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.nurkholiq.firebase_spotify.ui
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
+import com.nurkholiq.firebase_spotify.R
 import com.nurkholiq.firebase_spotify.adapters.SwipeSongAdapter
 import com.nurkholiq.firebase_spotify.data.entities.Song
 import com.nurkholiq.firebase_spotify.databinding.ActivityMainBinding
+import com.nurkholiq.firebase_spotify.exoplayer.isPlaying
 import com.nurkholiq.firebase_spotify.exoplayer.toSong
 import com.nurkholiq.firebase_spotify.other.Status.*
 import com.nurkholiq.firebase_spotify.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,6 +32,8 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var glide: RequestManager
+
+    private var playbackState: PlaybackStateCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,6 +78,35 @@ class MainActivity : AppCompatActivity() {
             curPlayingSong = it.toSong()
             glide.load(curPlayingSong?.imageUrl).into((binding.ivCurSongImage))
             switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+        }
+        mainViewModel.playbackState.observe(this) {
+            playbackState = it
+
+            binding.ivPlayPause.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+        }
+        mainViewModel.isConnected.observe(this) {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        rootLayout, result.message ?: "An unknown error occurred",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    else -> Unit
+                }
+            }
+        }
+        mainViewModel.networkError.observe(this) {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        rootLayout, result.message ?: "An unknown error occurred",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    else -> Unit
+                }
+            }
         }
     }
 }
