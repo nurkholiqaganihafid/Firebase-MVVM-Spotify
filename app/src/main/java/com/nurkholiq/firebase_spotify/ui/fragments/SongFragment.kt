bbe05b63635eb8e53642ec653c8883db662a9fd7
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-11-02 16:12:10 +0700

    create new class SongFragment untuk navigate ke SongFragment dan load information tentang lagu yang sedang diputar di TextViews dan ImageViews

---
+++ app/src/main/java/com/nurkholiq/firebase_spotify/ui/fragments/SongFragment.kt
@@ -0,0 +1,80 @@
+package com.nurkholiq.firebase_spotify.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.RequestManager
+import com.nurkholiq.firebase_spotify.R
+import com.nurkholiq.firebase_spotify.data.entities.Song
+import com.nurkholiq.firebase_spotify.databinding.FragmentSongBinding
+import com.nurkholiq.firebase_spotify.exoplayer.toSong
+import com.nurkholiq.firebase_spotify.other.Status.SUCCESS
+import com.nurkholiq.firebase_spotify.ui.viewmodels.MainViewModel
+import com.nurkholiq.firebase_spotify.ui.viewmodels.SongViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SongFragment : Fragment(R.layout.fragment_song) {
+
+    private var _binding: FragmentSongBinding? = null
+    private val binding get() = _binding!!
+
+    @Inject
+    lateinit var glide: RequestManager
+
+    private lateinit var mainViewModel: MainViewModel
+    private val songViewModel: SongViewModel by viewModels()
+
+    private var curPlayingSong: Song? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSongBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        subscribeToObservers()
+    }
+
+    private fun updateTitleAndSongImage(song: Song) {
+        val title = "${song.title} - ${song.subtitle}"
+        binding.tvSongName.text = title
+        glide.load(song.imageUrl).into(binding.ivSongImage)
+    }
+
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(viewLifecycleOwner) {
+            it?.let { result ->
+                when (result.status) {
+                    SUCCESS -> {
+                        result.data?.let { songs ->
+                            if (curPlayingSong == null && songs.isNotEmpty()) {
+                                curPlayingSong = songs[0]
+                                updateTitleAndSongImage(songs[0])
+                            }
+                        }
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        mainViewModel.curPlayingSong.observe(viewLifecycleOwner) {
+            if (it == null) return@observe
+            curPlayingSong = it.toSong()
+            updateTitleAndSongImage(curPlayingSong!!)
+        }
+    }
+}
